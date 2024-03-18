7dfe7156c82a34bfaf923b6b49eb23aa4dd727a4
AnthonyNDG <anthonyninodeguzman@gmail.com>
2024-03-18 19:44:30 -0400

    Added cards that connect to Database with Nextjs

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,7 +1,11 @@
+import { Flex, Grid, Text, Button, Card, Avatar, Box, Strong } from '@radix-ui/themes';
 import React, {useEffect, useState}from 'react'
 import { supabase } from '../lib/initSupabase';
+import ProgramCard from './components/Card'
 
 type Program = {
+  url_of_online_application: string;
+  program_description: string;
   unique_id_number: string;
   program_name: string;
   program_category: string;
@@ -32,17 +36,21 @@ function index() {
   }, []);
 
   return (
-    <div>
+    
+    <article>
+      <Text size='9' align='center' as='h1'> <strong>GovFind</strong> </Text>
+      <Text size='5' align='center' as='h2'><strong> Mission: Make applying for government programs in NYS easier and more accessible</strong></Text>
       {message}
-      <ul>
+      <ul style={{width:'90%', margin:"0 auto"}}>
         {programs.map((program) => (
-          <li key={program.unique_id_number}> 
-            {program.program_name} - 
-            {program.program_category}
+          <li key={program.unique_id_number} style={{margin:'20px 0',padding:'20px', border: "black solid 2px"}}> 
+            <ProgramCard name={program.program_name} desc={program.program_description} url={program.url_of_online_application} />
+            {/* {program.program_name} -
+            {program.program_category} */}
           </li>
         ))}
       </ul>
-    </div>
+    </article>
   );
 }
   // useEffect(() => {
